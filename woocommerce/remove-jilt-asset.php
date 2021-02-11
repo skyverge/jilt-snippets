e37bed7dfbfd7c67b9eb4ad48794fa37dbc7c352
rjfrizz <ryan@skyverge.com>
2021-02-11 11:51:59 -0500

    Create remove-jilt-asset.php

---
+++ woocommerce/remove-jilt-asset.php
@@ -0,0 +1,11 @@
+/* Prevents loading of admin-ajax requests on non WC pages. For edd see https://gist.github.com/jonathanpike/43dd9ce7a1b4cd0bea7407424f6edc7e */
+
+add_action( 'wp_print_scripts', function() {
+
+    if ( ! function_exists( 'is_woocommerce' ) ) return;
+
+    if ( ! is_woocommerce() && ! is_cart() && ! is_checkout() ) {
+        wp_dequeue_script( 'wc-jilt' );
+        wp_dequeue_script( 'wc-jilt-subscribe-form' );
+    }
+} );
