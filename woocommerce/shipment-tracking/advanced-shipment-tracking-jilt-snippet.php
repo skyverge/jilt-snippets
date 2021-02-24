5d42b734c721784589806c8e8db7aecef7452cc5
rjfrizz <ryan@skyverge.com>
2021-02-24 15:00:33 -0500

    updates advanced shipment tracking Jilt code

--- woocommerce/shipment-tracking/advanced-shipment-tracking-jilt-snippet.php
+++ woocommerce/shipment-tracking/advanced-shipment-tracking-jilt-snippet.php
@@ -3,31 +3,40 @@
 /**
  * Adds a tracking_code property to the order data used to update a Jilt order via the API.
  * 
- * Tracking code snippet specifically for use with the Advanced Shipment Tracking plugin: https://www.zorem.com/docs/woocommerce-advanced-shipment-tracking
- * See https://www.zorem.com/docs/woocommerce-advanced-shipment-tracking/developers/#how-to-get-tracking-information-of-order-programatically
- * 
  * You can insert the value of that property into a Jilt email using Liquid: http://help.jilt.com/en/articles/1288803-what-is-liquid-and-why-is-it-magic
  *
  * @param array $params order data
  * @param \WC_Jilt_Order $order order object
  * @return array
  */
-function sv_add_tracking_info_to_jilt_order_params( $params, $order ) {
-	if ( !is_plugin_active( 'woo-advanced-shipment-tracking/woocommerce-advanced-shipment-tracking.php' ) ) {
-	  return $params;
-	}
-	$shipment_tracking = WC_Advanced_Shipment_Tracking_Actions::get_instance();
 
-	$tracking_items = $shipment_tracking->get_tracking_items( $order->get_id(), true );
-	if ( ! empty( $tracking_items ) ) {
-		if ( ! isset( $params['properties'] ) ) {
-    		$params['properties'] = [];
-    	}
-		// use {{ order.properties.tracking[0].tracking_code }} to access the first providers tracking_code and insert it in a Jilt email
-		$params['properties']['tracking'] = $tracking_items;
-		// use {{ order.properties.tracking_code }} to access the tracking_number and insert it in a Jilt email
-		$params['properties']['tracking_code'] = $tracking_items[0]['tracking_number'];
+function sv_add_tracking_info_to_jilt_order_params( $params, $order ) {
+
+	if ( class_exists( 'WC_Advanced_Shipment_Tracking_Actions' ) ) {
+
+		$order_id = $order->get_id();
+
+		if ( function_exists( 'ast_get_tracking_items' ) ) {
+			$tracking_items = ast_get_tracking_items($order_id);
+
+			if ( ! empty( $tracking_items ) ) {
+				if ( ! isset( $params['properties'] ) ) {
+					$params['properties'] = [];
+				}
+			}		
+			
+			$params['properties']['tracking_code'] = $tracking_items[0]['tracking_number'];
+			$params['properties']['tracking_url'] = $tracking_items[0]['formatted_tracking_link'];
+
+			foreach($tracking_items as $tracking_item){		
+				$params['properties']['tracking']['tracking_code'] = $tracking_item['tracking_number'];
+				$params['properties']['tracking']['tracking_provider'] = $tracking_item['formatted_tracking_provider'];
+				$params['properties']['tracking']['tracking_url'] = $tracking_item['formatted_tracking_link'];							
+			}
+
+		}	
 	}
+
     return $params;
 }
 add_filter( 'wc_jilt_order_params', 'sv_add_tracking_info_to_jilt_order_params', 10, 2 );
